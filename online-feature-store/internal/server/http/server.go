3a32fa5502a16b40cd27f59b9494018e6df20000
Shivam Kumar <shivam.kumar@meesho.com>
2025-05-28 17:47:58 +0530

    feat: add http server implementation

---
+++ online-feature-store/internal/server/http/server.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
+var (
+	router *gin.Engine
+	once   sync.Once
+)
+
+func Init() {
+	once.Do(func() {
+		env := viper.GetString("APP_ENV")
+		if env == "prod" || env == "production" {
+			gin.SetMode(gin.ReleaseMode)
+		}
+		router = gin.New()
+		router.GET("/health/self", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "true"})
+		})
+	})
+}
+
+func Instance() *gin.Engine {
+	if router == nil {
+		log.Fatal().Msg("Router not initialized")
+	}
+	return router
+}
