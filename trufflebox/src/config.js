26da1a0fa63de19f357872e94e7a957556039c22
ati-jn <atishay.jain@meesho.com>
2025-06-04 13:42:30 +0530

    fix: runtime env.js creation

--- trufflebox/src/config.js
+++ trufflebox/src/config.js
@@ -1,4 +1,5 @@
-
 const env = window.env || {};
 
-export const REACT_APP_HORIZON_BASE_URL = env.REACT_APP_HORIZON_BASE_URL || "http://localhost:8082";
+export const REACT_APP_HORIZON_BASE_URL = process.env.REACT_APP_HORIZON_BASE_URL || env.REACT_APP_HORIZON_BASE_URL || "http://localhost:8082";
+export const REACT_APP_SKYE_BASE_URL = process.env.REACT_APP_SKYE_BASE_URL || env.REACT_APP_SKYE_BASE_URL || "http://localhost:8083";
+export const REACT_APP_MODEL_INFERENCE_BASE_URL = process.env.REACT_APP_MODEL_INFERENCE_BASE_URL || env.REACT_APP_MODEL_INFERENCE_BASE_URL || "http://localhost:8084";
