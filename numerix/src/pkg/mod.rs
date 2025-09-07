a039d4f4ea3bbc69878cec62a606226507571495
ansh-meesho <ansh.agrawal@meesho.com>
2025-09-07 22:43:47 +0530

    feat: Matrix operation service numerix (#172)

---
+++ numerix/src/pkg/mod.rs
@@ -0,0 +1,6 @@
+pub mod config;
+pub mod middleware;
+pub mod metrics;
+pub mod etcd;
+pub mod logger;
+pub mod rust_matrix_frame;
---
+++ numerix/src/pkg/rust_matrix_frame/mod.rs
@@ -0,0 +1,5 @@
+pub mod vector;
+pub mod ops;
+pub mod matrix;
+pub mod error;
+mod tests;
