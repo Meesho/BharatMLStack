e01cb14b73018700161f121ff8ffd39be0a0df68
addloopy <addloopy@gmail.com>
2025-09-22 08:03:52 +0530

    hnsw

---
+++ lv2l/parquet-reader/build/CMakeFiles/pqread.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pqread.dir/main.cpp.o"
+  "CMakeFiles/pqread.dir/main.cpp.o.d"
+  "pqread"
+  "pqread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pqread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
