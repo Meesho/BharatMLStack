421feb1c86fc63fb7a3d27f4bd3cc0506a53a0dd
ansh-meesho <ansh.agrawal@meesho.com>
2025-11-18 15:23:10 +0530

    fix

--- horizon/internal/numerix/config/models.go
+++
@@ -1,9 +0,0 @@
-package etcd
-
-type NumerixConfig struct {
-	Expression string `json:"expression"`
-}
-
-type NumerixConfigRegistery struct {
-	ExpressionConfig map[string]NumerixConfig `json:"expression-config"`
-}
