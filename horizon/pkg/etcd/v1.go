93c845fe8fbad4dc38ccf500a0760378da5217ff
ansh-meesho <ansh.agrawal@meesho.com>
2025-11-18 15:31:41 +0530

    fix

--- horizon/pkg/etcd/v1.go
+++ horizon/pkg/etcd/v1.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/rs/zerolog/log"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -632,47 +631,4 @@ func (v *V1) DeleteNode(path string) error {
 		return err
 	}
 	return nil
-}
-
-func newV1EtcdFromCustomPath(config interface{}, basePath string, appName string) Etcd {
-	if len(envEtcdServer) == 0 {
-		log.Panic().Msgf("ETCD_SERVER is not set")
-	}
-	etcdServers := envEtcdServer
-	servers := strings.Split(etcdServers, ",")
-	var username, password string
-	if len(envEtcdUsername) != 0 && len(envEtcdPassword) != 0 {
-		username = envEtcdUsername
-		password = envEtcdPassword
-	}
-
-	conn, err := clientv3.New(clientv3.Config{
-		Endpoints:           servers,
-		Username:            username,
-		Password:            password,
-		DialTimeout:         timeout * time.Second,
-		DialKeepAliveTime:   timeout * time.Second,
-		PermitWithoutStream: true,
-	})
-	if err != nil {
-		log.Error().Msgf("failed to create etcd client: %v", err)
-	}
-
-	v1Etcd := &V1{
-		conn:               conn,
-		basePath:           basePath + "/" + appName,
-		config:             config,
-		appName:            appName,
-		WatchPathCallbacks: make(map[string][]interface{}),
-	}
-	err = v1Etcd.updateConfig(config)
-
-	if envWatcherEnabled {
-		v1Etcd.WatchPrefix(context.Background(), basePath, appName)
-	}
-
-	if err != nil {
-		log.Panic().Err(err).Msgf("unable to create config from etcd")
-	}
-	return v1Etcd
 }
