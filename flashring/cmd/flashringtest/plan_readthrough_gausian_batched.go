5f1aaa213e275cfb3dd1bc94f690a41f991216d9
nileshsolankimeesho <nilesh.solanki@meesho.com>
2025-12-09 15:02:51 +0530

    read batching tests

--- flashring/cmd/flashringtest/plan_readthrough_gausian_batched.go
+++ flashring/cmd/flashringtest/plan_readthrough_gausian_batched.go
@@ -55,7 +55,7 @@ func planReadthroughGaussianBatched() {
 	flag.StringVar(&cpuProfile, "cpuprofile", "", "write cpu profile to this file")
 
 	flag.BoolVar(&enableBatching, "enable-batching", true, "enable read batching")
-	flag.IntVar(&batchWindowMicros, "batch-window-us", 1, "batch window in microseconds")
+	flag.IntVar(&batchWindowMicros, "batch-window-us", 0, "batch window in microseconds")
 	flag.IntVar(&maxBatchSize, "max-batch", 1000, "max batch size")
 	flag.Parse()
 
