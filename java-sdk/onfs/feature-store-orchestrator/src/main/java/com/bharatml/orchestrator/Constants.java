0ac0bfb1fec9d0cb1a3475fdcdcafc370f245736
ansh-meesho <ansh.agrawal@meesho.com>
2025-12-19 13:47:05 +0530

    Renamed from orchestrator to client and updated the readme

--- java-sdk/onfs/feature-store-orchestrator/src/main/java/com/bharatml/orchestrator/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.bharatml.orchestrator;
-
-public class Constants {
-    public static final String ONFS_GRPC_PROPERTIES = "onfsGrpcProperties";
-    public static final String ONFS_GRPC_CHANNEL_TEMPLATE = "onfsChannelGrpcTemplate";
-    public static final String ONFS_MONITORING_CLIENT_INTERCEPTOR_TEMPLATE = "monitoringClientInterceptor";
-    public static final String ONFS_CALLER_ID = "online-feature-store-caller-id";
-    public static final String ONFS_AUTH_TOKEN = "online-feature-store-auth-token";
-}
