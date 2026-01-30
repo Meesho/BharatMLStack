b52801146169b644d8fcca5955a864320c50c54d
Adit2607 <106079160+Adit2607@users.noreply.github.com>
2026-01-30 13:57:58 +0530

    Remove EP UI (#144)

--- trufflebox-ui/src/config.js
+++ trufflebox-ui/src/config.js
@@ -22,11 +22,9 @@ export const REACT_APP_ONLINE_FEATURE_STORE_ENABLED = getBooleanEnv('REACT_APP_O
 export const REACT_APP_INFERFLOW_ENABLED = getBooleanEnv('REACT_APP_INFERFLOW_ENABLED', true);
 export const REACT_APP_NUMERIX_ENABLED = getBooleanEnv('REACT_APP_NUMERIX_ENABLED', true);
 export const REACT_APP_PREDATOR_ENABLED = getBooleanEnv('REACT_APP_PREDATOR_ENABLED', true);
-export const REACT_APP_EMBEDDING_PLATFORM_ENABLED = getBooleanEnv('REACT_APP_EMBEDDING_PLATFORM_ENABLED', true);
 
 // Feature flag helper functions
 export const isOnlineFeatureStoreEnabled = () => REACT_APP_ONLINE_FEATURE_STORE_ENABLED;
 export const isInferFlowEnabled = () => REACT_APP_INFERFLOW_ENABLED;
 export const isNumerixEnabled = () => REACT_APP_NUMERIX_ENABLED;
 export const isPredatorEnabled = () => REACT_APP_PREDATOR_ENABLED;
-export const isEmbeddingPlatformEnabled = () => REACT_APP_EMBEDDING_PLATFORM_ENABLED;
--- trufflebox-ui/src/pages/EmbeddingPlatform/components/DeploymentOperations/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DeploymentDashboard from './DeploymentDashboard';
-
-const DeploymentOperations = () => {
-  return <DeploymentDashboard />;
-};
-
-export default DeploymentOperations;
